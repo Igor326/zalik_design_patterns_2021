bab83114f7f3b8a1cfdda1ff73a51c8816f48d5a
Igor326 <71424088+Igor326@users.noreply.github.com>
2021-06-07 20:42:53 +0300

    Changed minor things

---
+++ DOzalik Lukashenko Igor/DOzalik Lukashenko Igor/CashMachineBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DOzalik_Lukashenko_Igor
+{
+    public abstract class CashMachineBase//Базовий клас для пристроїв друку грошей 
+    {
+        
+        public string Name { get; protected set; }//Назва пристрою
+
+        public CashMachineBase(string name)/// Конструктор для створення нового екземпляру пристрою для друку .
+        {
+            // Перевіряємо дані на корректність.
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            // Встановлюємо значення.
+            Name = name;
+        }
+        
+        public abstract MoneyBase[] Create(int pageCount);//Надрукувати нову партію грошей
+
+        
+        public override string ToString() // Приводимо об'єкт до String'а.
+        {
+            return Name;
+        }
+    }
+}
---
+++ DOzalik Lukashenko Igor/DOzalik part 2/Location.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DOzalik_part_2
+{
+    class Location
+    {
+
+    }
+}
